7e4b8ee3635954bc45afe18c93edf7e88dee7aac
Mo Arefi <m-arefi@live.com>
2022-08-29 00:15:52 +0200

    Dotenv and server updates

--- server/server.js
+++ server/server.js
@@ -1,17 +1,26 @@
+require("dotenv").config()
 const express = require("express");
 const SpotifyWebApiNode = require("spotify-web-api-node");
+const cors = require("cors")
+const lyricsFinder = require("lyrics-finder")
+const bodyParser = require("body-parser")
+
 
 const app = express();
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.post('/login', (req, res) => {
-  const code = req.body.code
-  const spotifyApi = new SpotifyWebApiNode({
-    clientId: 'fcecfc72172e4cd267473117a17cbd4d',
-    clientSecret: 'a6338157c9bb5ac9c71924cb2940e1a7',
-    redirectUri: 'http://localhost:3000',
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken,
   })
 
-spotifyApi
+  spotifyApi
     .refreshAccessToken()
     .then(data => {
       res.json({
@@ -24,3 +33,33 @@ spotifyApi
       res.sendStatus(400)
     })
 })
+
+app.post("/login", (req, res) => {
+  const code = req.body.code
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+  })
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then(data => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      })
+    })
+    .catch(err => {
+      res.sendStatus(400)
+    })
+})
+
+app.get("/lyrics", async (req, res) => {
+  const lyrics =
+    (await lyricsFinder(req.query.artist, req.query.track)) || "No Lyrics Found"
+  res.json({ lyrics })
+})
+
+app.listen(3000)
