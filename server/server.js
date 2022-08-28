bc48017f6fa75987cb1862a559b48b4ea8d627fb
Mo Arefi <m-arefi@live.com>
2022-08-28 23:35:00 +0200

    ServerJS Error caught

--- server/server.js
+++ server/server.js
@@ -4,9 +4,23 @@ const SpotifyWebApiNode = require("spotify-web-api-node");
 const app = express();
 
 app.post('/login', (req, res) => {
-  const spotifyApi = new.SpotifyWebApiNode({
+  const code = req.body.code
+  const spotifyApi = new SpotifyWebApiNode({
     clientId: 'fcecfc72172e4cd267473117a17cbd4d',
     clientSecret: 'a6338157c9bb5ac9c71924cb2940e1a7',
     redirectUri: 'http://localhost:3000',
   })
+
+spotifyApi
+    .refreshAccessToken()
+    .then(data => {
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn,
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.sendStatus(400)
+    })
 })
