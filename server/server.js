0aa21822ae29757dab20a95d4367b6504df2de79
Mo Arefi <m-arefi@live.com>
2022-08-28 23:28:52 +0200

    .env test

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const SpotifyWebApi = require("spotify-web-api-node");
+const SpotifyWebApiNode = require("spotify-web-api-node");
 
 const app = express();
 
 app.post('/login', (req, res) => {
-  const spotifyApi = new.SpotifyWebApi({
-    redirectUri: 'http://localhost:3000',
+  const spotifyApi = new.SpotifyWebApiNode({
     clientId: 'fcecfc72172e4cd267473117a17cbd4d',
     clientSecret: 'a6338157c9bb5ac9c71924cb2940e1a7',
+    redirectUri: 'http://localhost:3000',
   })
 })
