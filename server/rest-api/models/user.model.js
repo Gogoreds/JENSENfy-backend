cdd4b07cba538625568dc6dab33e6db95520b784
Abdullahi M. Ibrahim <abdullah98@hotmail.se>
2022-08-30 19:20:55 +0200

    REST-api done

---
+++ server/rest-api/models/user.model.js
@@ -0,0 +1,17 @@
+module.exports = mongoose => {
+  var schema = mongoose.Schema(
+    {
+      userName: String,
+      password: String,
+      published: Boolean
+    },
+    { timestamps: true }
+  );
+  schema.method("toJSON", function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+  const User = mongoose.model("user", schema);
+  return User;
+};
