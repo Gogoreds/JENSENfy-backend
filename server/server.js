9b854704d76ea5a935218b2b63680e217917b2e3
Abdullahi M. Ibrahim <abdullah98@hotmail.se>
2022-09-06 16:51:52 +0200

    clean up

--- server/server.js
+++ server/server.js
@@ -1,12 +1,45 @@
 require("dotenv").config()
 const express = require("express");
-const SpotifyWebApiNode = require("spotify-web-api-node");
-const cors = require("cors")
+const cors = require("cors");
 const lyricsFinder = require("lyrics-finder")
 const bodyParser = require("body-parser")
-
+const SpotifyWebApiNode = require("spotify-web-api-node");
 
 const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "users database" });
+});
+
+require("./routes/user.routes")(app);
+
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -62,4 +95,8 @@ app.get("/lyrics", async (req, res) => {
   res.json({ lyrics })
 })
 
-app.listen(3000)
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
