a46ae27a90afd29a6f99b2313515a3d76f34fecf
Mohammad Arefi <m-arefi@live.com>
2022-09-28 10:05:45 +0200

    SpotifyWebApiNode

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const lyricsFinder = require("lyrics-finder")
-const bodyParser = require("body-parser")
+const lyricsFinder = require("lyrics-finder");
+const bodyParser = require("body-parser");
 const SpotifyWebApiNode = require("spotify-web-api-node");
 const app = express();
-const swaggerUi = require('swagger-ui-express'),
-  swaggerDocument = require('./swagger.json');
+const swaggerUi = require("swagger-ui-express"),
+  swaggerDocument = require("./swagger.json");
 
 const corsOptions = {
-  origin: "http://localhost:3000"
+  origin: "http://localhost:3000",
 };
 
 app.use(cors(corsOptions));
@@ -24,12 +24,12 @@ const db = require("./models");
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Connected to the database!");
   })
-  .catch(err => {
+  .catch((err) => {
     console.log("Cannot connect to the database!", err);
     process.exit();
   });
@@ -41,66 +41,63 @@ app.get("/", (req, res) => {
 
 require("./routes/user.routes")(app);
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/refresh", (req, res) => {
-  const refreshToken = req.body.refreshToken
-  const spotifyApi = new spotifyWebApiNode({
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApiNode({
     redirectUri: process.env.REDIRECT_URI,
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     refreshToken,
-  })
+  });
 
   spotifyApi
     .refreshAccessToken()
-    .then(data => {
+    .then((data) => {
       res.json({
         accessToken: data.body.accessToken,
         expiresIn: data.body.expiresIn,
-      })
+      });
     })
-    .catch(err => {
-      console.log(err)
-      res.sendStatus(400)
-    })
-})
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
 
 app.post("/login", (req, res) => {
-  const code = req.body.code
-  const spotifyApi = new spotifyWebApiNode({
+  const code = req.body.code;
+  const spotifyApi = new SpotifyWebApiNode({
     redirectUri: process.env.REDIRECT_URI,
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-  })
+  });
 
   spotifyApi
     .authorizationCodeGrant(code)
-    .then(data => {
+    .then((data) => {
       res.json({
         accessToken: data.body.access_token,
         refreshToken: data.body.refresh_token,
         expiresIn: data.body.expires_in,
-      })
+      });
     })
-    .catch(err => {
-      res.sendStatus(400)
-    })
-})
+    .catch((err) => {
+      res.sendStatus(400);
+    });
+});
 
 app.get("/lyrics", async (req, res) => {
   const lyrics =
-    (await lyricsFinder(req.query.artist, req.query.track)) || "No Lyrics Found"
-  res.json({ lyrics })
-})
+    (await lyricsFinder(req.query.artist, req.query.track)) ||
+    "No Lyrics Found";
+  res.json({ lyrics });
+});
 
-app.use(
-  '/api-docs',
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerDocument)
-);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
