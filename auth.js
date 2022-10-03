75c3b0db697faa345477959198b018817efbeb7f
Abdullahi M. Ibrahim <abdullah98@hotmail.se>
2022-10-04 01:10:03 +0200

    auth work done

--- auth.js
+++ auth.js
@@ -1,25 +1,47 @@
-const jwt = require("jsonwebtoken");
+const db = require("./models/");
+const User = db.users;
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-module.exports = async (request, response, next) => {
-  try {
-    //   get the token from the authorization header
-    const token = await request.headers.authorization.split(" ")[1];
 
-    //check if the token matches the supposed origin
-    const decodedToken = await jwt.verify(token, "RANDOM-TOKEN");
+const handleLogin = async (req, res) => {
+  const { userName, password } = req.body;
+  if (!userName || !password) return res.status(400).json({ 'message': 'Username and password are required.' });
 
-    // retrieve the user details of the logged in user
-    const user = await decodedToken;
+  const foundUser = await User.findOne({ userName }).exec();
+  if (!foundUser) return res.sendStatus(401); //Unauthorized 
+  // evaluate password 
+  const match = await bcrypt.compare(password, foundUser.password);
+  if (match) {
+    // create JWTs
+    const accessToken = jwt.sign(
+      {
+        "UserInfo": {
+          "userName": foundUser.userName,
+        }
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: '45s' }
+    );
+    const refreshToken = jwt.sign(
+      { "userName": foundUser.userName },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: '1d' }
+    );
+    // Saving refreshToken with current user
+    foundUser.refreshToken = refreshToken;
+    const result = await foundUser.save();
+    console.log(result);
 
-    // pass the user down to the endpoints here
-    request.user = user;
+    // Creates Secure Cookie with refresh token
+    res.cookie('jwt', refreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
 
-    // pass down functionality to the endpoint
-    next();
-    
-  } catch (error) {
-    response.status(401).json({
-      error: new Error("Invalid request!"),
-    });
+    // Send authorization and access token to user
+    res.json({ accessToken });
+
+  } else {
+    res.sendStatus(401);
   }
-};
+}
+
+module.exports = { handleLogin };
